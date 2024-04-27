ba80fcf645970b7902c55c74ffffdc3773bcfb28
Deepak Sharma <dsharma2828@gmail.com>
2024-04-27 10:45:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstChar(text) {
-  // your code here
-}
-
-// Do not change the code below
-
-const text = prompt("Enter text:");
-alert(firstChar(text));
+    // your code here
+    for(let i = 0; i < text.length; i++){
+      if(text[i] !== " "){
+        return text[i];
+      }
+    }
+    return "";
+  }
+  
+  // Do not change the code below
+  
+  const text = prompt("Enter text:");
+  alert(firstChar(text));
+  
